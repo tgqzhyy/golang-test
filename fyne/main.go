8e166a6756b6354be8233f54590469ea19e60437
landv <root@landv.pw>
2019-10-14 18:00:11 +0800

    fyne

--- fyne/main.go
+++ fyne/main.go
@@ -5,11 +5,17 @@ import (
 	"fyne.io/fyne/widget"
 	"fyne.io/fyne/app"
 )
+/**
+export FYNE_FONT=/usr/share/fonts/fonts-cesi/CESI_SS_GB18030.TTF
+echo $FYNE_FONT
+go run main.go
+ */
 var b fyne.Size
 func main() {
 
 	app := app.New()
 	w := app.NewWindow("标题中文测试")
+
 	w.CenterOnScreen()
 	b.Height=150
 	b.Width=300
