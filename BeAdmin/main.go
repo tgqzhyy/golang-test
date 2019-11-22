c64a359335542a3cd2d1fb12d8edf2bd22d23a21
landv <root@landv.pw>
2019-11-22 10:50:06 +0800

    BeAdmin

---
+++ BeAdmin/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "golang-test/BeAdmin/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ BeAdmin/routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"golang-test/BeAdmin/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
