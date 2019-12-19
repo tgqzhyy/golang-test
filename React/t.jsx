157362e170ac1cbcb42df0f6ad001be038b2f6c5
landv <root@landv.pw>
2019-12-19 21:28:35 +0800

    https://github.com/davyxu/cellnet cellnet经过多个版本的迭代，无论是作为初学者学习的范例，还是作为私用、商用项目的基础构建乃至核心技术层已经在业内广受了解及使用。
    主要使用领域：
    
    游戏服务器
    
    方便定制私有协议，快速构建逻辑服务器、网关服务器、服务器间互联互通、对接第三方SDK、转换编码协议等
    
    ARM设备
    
    设备间网络通讯
    
    证券软件
    
    内部RPC

--- React/t.jsx
+++ React/t.jsx
@@ -59,3 +59,51 @@ function sum(a,b) {
 function withdraw(account,amount) {
     account.total -=amount;
 }
+
+/**
+ *
+ */
+
+function WarningBanner(props) {
+    if (!props.warn){
+        return null;
+    }
+
+    return (
+        <div className="warning">
+            警告！
+        </div>
+    );
+}
+
+class Page extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={showWarning:true}
+        this.handleToggleClick =this.handleToggleClick.bind(this)
+    }
+
+    handleToggleClick(){
+        this.setState(prevState=>(
+            {
+                showWarning: !prevState.showWarning
+            }
+        ));
+    }
+
+    render(){
+        return(
+            <div>
+                <WarningBanner warn={this.state.showWarning}></WarningBanner>
+                <button onClick={this.handleToggleClick}>
+                    {this.state.showWarning?'隐藏':'显示'}
+                </button>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(
+    <Page></Page>,
+    document.getElementById('example')
+)
